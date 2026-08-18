a564cb8a6dbcebd8071384d391b5e7527c9620a1
catkiss62 <aolu0610@gmail.com>
2026-08-19 00:29:11 +0800

    test: assert equal posture on equal profile

--- app/test/personality_trial_test.dart
+++ app/test/personality_trial_test.dart
@@ -71,7 +71,7 @@ void main() {
     expect(trial, contains('内在反应'));
     expect(trial, contains('表达过滤'));
     expect(trial, contains('可见思考默认用第一人称“我”'));
-    expect(trial, contains('他是平等的男朋友'));
+    expect(reserved, contains('他是平等的男朋友'));
     expect(trial, contains('反咬一口'));
     expect(trial, contains('抓住破绽追一下'));
     expect(trial, isNot(contains('当前试穿性格')));
