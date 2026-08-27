bd2c79e58caff2ad8a58e80b8330487813eebee9
catkiss62 <aolu0610@gmail.com>
2026-08-27 14:26:32 +0900

    Fix v0.39.2 analyze: app/test/immersive_room_model_test.dart

--- app/test/immersive_room_model_test.dart
+++ app/test/immersive_room_model_test.dart
@@ -1,4 +1,4 @@
-import 'package:ai_companion/core/models/immersive_room.dart';
+import 'package:ai_companion_localfirst/core/models/immersive_room.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
